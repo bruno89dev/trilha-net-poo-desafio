51763cf3551b4749779d2843daf770292912032f
bruno89dev <bruno89dev@gmail.com>
2022-09-29 22:13:52 -0300

    ProjetoFinalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Smartphone Nokia");
+            Nokia nokia = new Nokia("998887766", "5.4", "222333444", 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Instagram");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone Iphone");
+            Iphone iphone13 = new Iphone("998887755", "13", "444333222", 128);
+            iphone13.ReceberLigacao();
+            iphone13.InstalarAplicativo("Facebook");
+            
+        }
+    }
+}
